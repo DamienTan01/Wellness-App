81c2df61ec64fc995c04014a05901bda43c4a63e
Damien Tan <dtan0385@gmail.com>
2022-07-20 23:08:02 +0800

    Updates to UI

--- WellnessApp/app/src/main/java/com/example/wellnessapp/login.java
+++ WellnessApp/app/src/main/java/com/example/wellnessapp/login.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -15,16 +14,15 @@ public class login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
-        TextView txtSign = findViewById(R.id.txtSignup);
-        txtSign.setOnClickListener(view -> {
-            Intent intent = new Intent(this, signup.class);
-
-            startActivity(intent);
-        });
-
         ImageButton login = findViewById(R.id.login);
         login.setOnClickListener(view -> {
             Intent intent = new Intent(this, home.class);
+            startActivity(intent);
+        });
+
+        TextView txtSign = findViewById(R.id.signupText);
+        txtSign.setOnClickListener(view -> {
+            Intent intent = new Intent(this, signup.class);
 
             startActivity(intent);
         });
--- WellnessApp/app/src/main/java/com/example/wellnessapp/signup.java
+++ WellnessApp/app/src/main/java/com/example/wellnessapp/signup.java
@@ -4,28 +4,46 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class signup extends AppCompatActivity {
+    ImageButton signup;
+    EditText txtUser, txtEmail, txtPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
 
-        TextView txtLogin = findViewById(R.id.txtLogin);
+        TextView txtLogin = findViewById(R.id.loginText);
         txtLogin.setOnClickListener(view -> {
             Intent intent = new Intent(this, login.class);
 
             startActivity(intent);
         });
 
+        //User Registration
+        txtUser = (EditText)findViewById(R.id.user);
+        txtEmail = (EditText)findViewById(R.id.email);
+        txtPass = (EditText)findViewById(R.id.pass);
         ImageButton signup = findViewById(R.id.signup);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = txtUser.getText().toString();
+                String email = txtEmail.getText().toString();
+                String password = txtPass.getText().toString();
+            }
+        });
         signup.setOnClickListener(view -> {
             Intent intent = new Intent(this, login.class);
-
             startActivity(intent);
+
+
         });
     }
 }
