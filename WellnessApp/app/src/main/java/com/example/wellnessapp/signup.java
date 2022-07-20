2790b64c4a926b741e18c3b2f52ceb50fdd6d617
Damien Tan <dtan0385@gmail.com>
2022-07-21 00:47:36 +0800

    Added navBar

--- WellnessApp/app/src/main/java/com/example/wellnessapp/signup.java
+++ WellnessApp/app/src/main/java/com/example/wellnessapp/signup.java
@@ -2,6 +2,7 @@ package com.example.wellnessapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,8 +11,6 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class signup extends AppCompatActivity {
-    ImageButton signup;
-    EditText txtUser, txtEmail, txtPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,29 +20,13 @@ public class signup extends AppCompatActivity {
         TextView txtLogin = findViewById(R.id.loginText);
         txtLogin.setOnClickListener(view -> {
             Intent intent = new Intent(this, login.class);
-
             startActivity(intent);
         });
 
-        //User Registration
-        txtUser = (EditText)findViewById(R.id.user);
-        txtEmail = (EditText)findViewById(R.id.email);
-        txtPass = (EditText)findViewById(R.id.pass);
-        ImageButton signup = findViewById(R.id.signup);
-
-        signup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String username = txtUser.getText().toString();
-                String email = txtEmail.getText().toString();
-                String password = txtPass.getText().toString();
-            }
-        });
-        signup.setOnClickListener(view -> {
+        ImageButton btnSignup = findViewById(R.id.btnSignup);
+        btnSignup.setOnClickListener(view -> {
             Intent intent = new Intent(this, login.class);
             startActivity(intent);
-
-
         });
     }
 }
